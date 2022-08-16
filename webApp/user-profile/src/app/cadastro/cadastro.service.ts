5fd9ae8fb876c1c04dcaf311cd753c88468a63aa
dantascaio <caioaires@gmail.com>
2022-08-15 22:46:16 -0300

    webapp added to compose

--- webApp/user-profile/src/app/cadastro/cadastro.service.ts
+++ webApp/user-profile/src/app/cadastro/cadastro.service.ts
@@ -4,71 +4,71 @@ import { Observable } from 'rxjs';
 import { SafeUrl } from '@angular/platform-browser';
 
 export interface IAllUsers {
-    name: string;
-    id: number;
-    images : IImages[];
-  }
-  export interface IImages {
-    base64: string;
-    id: number;
-    user_id: number;
-    base64Safe: SafeUrl
-  }
+  name: string;
+  id: number;
+  images: IImages[];
+}
+export interface IImages {
+  base64: string;
+  id: number;
+  user_id: number;
+  base64Safe: SafeUrl
+}
 
-  export interface INewUser {
-    name: string 
-    images: INewImages[];
-  }
+export interface INewUser {
+  name: string
+  images: INewImages[];
+}
 
-  export interface INewImages {
-    base64: Observable<any>;
-  }
+export interface INewImages {
+  base64: Observable<any>;
+}
 
-  export interface IImageAux{
-    base64: string;
-  }
+export interface IImageAux {
+  base64: string;
+}
 
 
 
 
 @Injectable({
-    providedIn: 'root',
-  })
+  providedIn: 'root',
+})
 
-  export class  CadastroService{
-    pathUrlBase = 'http://localhost:8000'
-    constructor(private http: HttpClient) {}
-
-
-
-    listarTodosUsuarios(): Observable<IAllUsers[]> {
-        return this.http.get<IAllUsers[]>(
-          `${this.pathUrlBase}/users`
-        );
-      }
-
-      listaUsuarioPorId(
-        id: number
-      ): Observable<IAllUsers> {
-        return this.http.post<IAllUsers>(
-          `${this.pathUrlBase}/users`,
-          { id }
-        );
-      }
+export class CadastroService {
+  pathUrlBase = 'http://localhost:8000'
+  constructor(private http: HttpClient) { }
 
 
 
-      cadastrarUsuario(
-        newUser: INewUser
-      ): Observable<IAllUsers> {
-        console.log(newUser)
-        return this.http.post<IAllUsers>(
-          `${this.pathUrlBase}/users`,
-          { ...newUser }
-        );
-      }
-
+  listarTodosUsuarios(): Observable<IAllUsers[]> {
+    return this.http.get<IAllUsers[]>(
+      `${this.pathUrlBase}/users`
+    );
   }
+
+  listaUsuarioPorId(
+    id: number
+  ): Observable<IAllUsers> {
+    return this.http.post<IAllUsers>(
+      `${this.pathUrlBase}/users`,
+      { id }
+    );
+  }
+
+
+
+  cadastrarUsuario(
+    newUser: INewUser
+  ): Observable<IAllUsers> {
+    console.log(newUser)
+    return this.http.post<IAllUsers>(
+      `${this.pathUrlBase}/users`,
+      { ...newUser }
+    );
+  }
+
+}
 
 
 
